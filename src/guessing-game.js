eec422e57d4d257be0fcc5d0a0d4e9de59e0feec
Jane Kulakovski <jkulakovski@gmail.com>
2017-01-29 18:32:43 +0300

    guessing-game:class is implemented

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+		this.mid = null;
+		this.first = null;
+		this.last = null;
+	}
 
     setRange(min, max) {
-
+		this.first = min;
+		this.last = max;
     }
 
     guess() {
-
+		this.mid = Math.round((this.first + this.last)/2);
+		return this.mid;
     }
 
     lower() {
-
+		this.last = this.mid;
     }
 
     greater() {
-
+        this.first = this.mid;
     }
 }
 
